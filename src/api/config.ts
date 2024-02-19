b5d01ec832c4d8cebce5ad947450b6d2a8aa4ee5
饶奎 <2955095425@qq.com>
2024-02-19 14:49:22 +0800

    更新环境

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,14 +1,14 @@
 const wsUrl ={
   dev:{
-    BASE_URL:  "http://localhost:55555",
+    BASE_URL:  "http://localhost:55556",
     ws:'ws://localhost:55555' ,
     TIMEOUT:1000 ,
   },
   env:{
-    BASE_URL:"http://49.234.58.117:55555",
+    BASE_URL:"http://49.234.58.117:55556",
     ws:'ws://49.234.58.117:55555' ,
     TIMEOUT: 1000 ,
   }
 }
 
-export default  wsUrl.dev
+export default  wsUrl.env
